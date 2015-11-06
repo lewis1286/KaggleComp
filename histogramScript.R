c349af5f73a3044efc7f112b0ecf9035815ddcde
vpberges <vpberges@DN0a224722.SUNet>
2015-11-06 12:45:39 -0800

    The new LAsso with the 50% ratio of NA features

--- histogramScript.R
+++ histogramScript.R
@@ -3,6 +3,28 @@
 training.features <- read.csv("../training_features.csv")
 t.is.na = is.na(training.features)
 his = colSums(t.is.na)
-plot(1:ncol(his),his,"h")
+plot(1:ncol(t.is.na),his,"h")
 
 
+#RATIO OF DEPLETION  CV on this tuning parameter ?
+ratio = 0.5
+
+training.features <- read.csv("../training_features.csv")
+validation.features <- read.csv("../validation_features.csv")
+leaderboard.features<- read.csv("../leaderboard_features.csv")
+
+all.patients = rbind(training.features, validation.features,leaderboard.features)
+
+hist.all.patients = colSums(is.na(all.patients))
+
+all.patients.size = nrow(all.patients)
+
+bad.features=NULL
+
+for (nam in names(all.patients)){
+  if (hist.all.patients[nam]>all.patients.size*ratio){
+    bad.features = c(bad.features,nam)
+  }
+}
+
+bad.features
